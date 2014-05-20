c17ae48a00b62fce33df58c93693d52cd05aeaa6
Jarrod Roberson <jarrod@vertigrated.com>
2014-05-20 01:54:40 -0400

    How to control construction and limit mutability

---
+++ src/main/java/com/stackoverflow/Q23751298.java
@@ -0,0 +1,35 @@
+package com.stackoverflow;
+
+import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Q23751298
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            final BufferedImage img = ImageIO.read(new File("/Users/jhr/Pictures/fu4FM.gif"));
+            final int match = new Color(209, 167, 86).getRGB();
+
+            for (int x = 0; x < img.getWidth(); x++)
+            {
+                for (int y = 0; y < img.getHeight(); y++)
+                {
+                    final int irgb = img.getRGB(x, y);
+                    if (irgb == match)
+                    {
+                        System.out.format("%d/%d = %d : %d\n", x, y, img.getRGB(x, y), match);
+                    }
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
