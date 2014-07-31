1f96fdbbf38fce860cfe4750dd99f630aee9b17e
Jarrod Roberson <jarrod@vertigrated.com>
2014-07-31 14:53:57 -0400

    updating answer to use guava and process alpha ranges as well as numeric ranges

--- src/main/java/com/stackoverflow/Q25004732.java
+++ src/main/java/com/stackoverflow/Q25004732.java
@@ -1,44 +1,48 @@
 package com.stackoverflow;
 
-import java.util.HashSet;
-import java.util.Set;
+import javax.annotation.Nonnull;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.google.common.collect.Range;
+import com.google.common.collect.RangeSet;
+import com.google.common.collect.TreeRangeSet;
+
 public class Q25004732
 {
-    private static final Pattern P = Pattern.compile("(\\d+)-(\\d+)");
+    private static final Pattern RANGE = Pattern.compile("(?i)^(?=[a-z]+-[a-z]+$|\\d+-\\d+$)([a-z\\d]+)-([a-z\\d]+)$");
 
     public static void main(final String[] args)
     {
-        final int input = 13;
-        final String[] sa = "1,3,10-15,17".split(",");
-        final Set<Integer> is = new HashSet<Integer>();
+        final String[] sa = "1,3,10-15,17,A,b,XX-ZZ,z".split(",");
+        final RangeSet<String> is = TreeRangeSet.create();
         for (final String s : sa)
         {
-            final Matcher m = P.matcher(s);
+            System.out.println("s = " + s);
+            final Matcher m = RANGE.matcher(s);
             if (m.find())
             {
-                final int start = Integer.parseInt(m.group(1));
-                final int end = Integer.parseInt(m.group(2));
-                for (int i = start; i <= end; i++)
-                {
-                    is.add(i);
-                }
+                is.add(Range.closed(m.group(1), m.group(2)));
             }
             else
             {
-                is.add(Integer.parseInt(s));
+                is.add(Range.closed(s, s));
             }
         }
+        report("13", is);
+        report("A", is);
+        report("XY", is);
+    }
 
-        if (is.contains(input))
+    private static void report(@Nonnull final String input, @Nonnull final RangeSet<String> rs)
+    {
+        if (rs.contains(input))
         {
-            System.out.format("Found %d in the set %s", input, is);
+            System.out.format("Found %s in the set %s", input, rs);
         }
         else
         {
-            System.out.format("Didn't find %d in set %s", input, is);
+            System.out.format("Didn't find %s in set %s", input, rs);
         }
         System.out.println();
     }
