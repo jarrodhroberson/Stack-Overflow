60f6c5d42efee86e8f828c9e99d45f659be3b2bf
Jarrod Roberson <jarrod@vertigrated.com>
2015-08-08 23:57:57 -0400

    adding a new version of an old answer

--- src/main/java/com/stackoverflow/Q6613429.java
+++ src/main/java/com/stackoverflow/Q6613429.java
@@ -7,13 +7,8 @@ public class Q6613429
 {
     public static void main(final String[] args)
     {
-        final Rights r = new Rights().grantUser("me").permissionTo("ALL").item("EVERYTHING").asOf(new Date());
+        final Rights r = PermissionManager.grantUser("me").permissionTo("ALL").item("EVERYTHING").asOf(new Date());
         PermissionManager.apply(r);
-    }
-
-    public static class PermissionManager
-    {
-        public static void apply(@Nonnull final Rights r) { /* do the persistence here */ }
     }
 
     public static class Rights
@@ -24,31 +19,49 @@ public class Q6613429
         private Date ofDate;
 
         private Rights() { /* intentionally blank */ }
+    }
 
-        public AssignPermission grantUser(@Nonnull final String user)
+    public static class PermissionManager
+    {
+        public static PermissionManager.AssignPermission grantUser(@Nonnull final String user)
         {
-            this.user = user; return new AssignPermission()
-        {
-            @Override
-            public AssignItem permissionTo(@Nonnull String p)
-            {
-                Rights.this.permission = p; return new AssignItem() {
+            final Rights r = new Rights(); return new AssignPermission() {
+
                 @Override
-                public SetDate item(String i) {
-                    Rights.this.item = i; return new SetDate()
+                public AssignItem permissionTo(@Nonnull String p) {
+                    r.permission = p;
+                    return new AssignItem()
                 {
                     @Override
-                    public Rights asOf(Date d) {
-                        Rights.this.ofDate = d; return Rights.this;
-                    }};
-                }};
-            }};
+                    public SetDate item(String i) {
+                        r.item = i;
+                        return new SetDate()
+                    {
+                        @Override
+                        public Rights asOf(Date d) {
+                            r.ofDate = d;
+                            return r;
+                        }
+                    };}
+                };}
+            };
         }
 
-        public interface AssignPermission { public AssignItem permissionTo(@Nonnull final String p); }
+        public static void apply(@Nonnull final Rights r) { /* do the persistence here */ }
 
-        public interface AssignItem { public SetDate item(String i); }
+        public interface AssignPermission
+        {
+            public AssignItem permissionTo(@Nonnull final String p);
+        }
 
-        public interface SetDate { public Rights asOf(Date d); }
+        public interface AssignItem
+        {
+            public SetDate item(String i);
+        }
+
+        public interface SetDate
+        {
+            public Rights asOf(Date d);
+        }
     }
 }
