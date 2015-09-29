20e5cb7ae5233841e86a218d6c3615c8cf2af322
Jarrod Roberson <jarrod@vertigrated.com>
2015-09-29 01:19:19 -0400

    adding an answer showing professional approach

--- src/main/java/com/stackoverflow/Q32835243.java
+++ src/main/java/com/stackoverflow/Q32835243.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Q32835243
 {
-    private static double farhenheitToCelsius(double fahrenheit) {return (fahrenheit - 32) * 5 / 9;}
+    private static double farhenheitToCelsius(final double fahrenheit) {return (fahrenheit - 32) * 5 / 9;}
 
     public static void main(final String[] args)
     {
@@ -18,7 +18,7 @@ public class Q32835243
             System.out.format("The temperature in Celsius is: %8.4f%n", celsius);
             System.out.format("Fahrenheit\t Celsius%n");
 
-            for (int i=0; i < 100; i+=10)
+            for (int i = 0; i < 100; i += 10)
             {
                 System.out.format("%8.4f\t%8.4f%n", fahrenheit + i, farhenheitToCelsius(fahrenheit + i));
             }
