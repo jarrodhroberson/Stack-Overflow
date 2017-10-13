4383e01bddd4d4ac69de7e559be16a3aca09b3b7
Jarrod Roberson <jarrod@vertigrated.com>
2017-10-12 23:46:44 -0400

    creating an actual typesafe value class for the Probe

---
+++ src/main/java/com/svd/StreamVsDecoratorAutoValued.java
@@ -0,0 +1,47 @@
+package com.svd;
+
+import com.google.auto.value.AutoValue;
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableList;
+
+import javax.annotation.Nonnull;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.google.common.collect.Iterables.*;
+
+public class StreamVsDecoratorAutoValued
+{
+    @AutoValue
+    public static abstract class IndexedDouble
+    {
+        static IndexedDouble create(@Nonnull final Integer index, @Nonnull final Double value)
+        {
+            return new AutoValue_StreamVsDecorator_IndexedDouble(index, value);
+        }
+
+        abstract Integer index();
+
+        abstract Double value();
+    }
+
+
+    public static void main(String[] args)
+    {
+        final Double ZERO = 0.0d;
+        final Double ONE = 1.0d;
+        final Iterable<Double> probes = ImmutableList.copyOf(new Random().doubles(1000, 0.0d, 1.0d).iterator());
+
+        transform(limit(filter(probes, probe -> ZERO.compareTo(probe) < 0 || ONE.compareTo(probe) > 0), 10), new Function<Double, IndexedDouble>()
+        {
+            final AtomicInteger index = new AtomicInteger(0);
+
+            @Nonnull
+            @Override
+            public IndexedDouble apply(@Nonnull final Double probe)
+            {
+                return IndexedDouble.create(index.incrementAndGet(), probe);
+            }
+        }).forEach(idp -> System.out.printf("Probe #%d: %f\n", idp.index(), idp.value() * 100.0d));
+    }
+}
